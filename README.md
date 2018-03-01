c679c3987641421f80825c8127ca2641653d5000
pengganyu <peng_gy@163.com>
2018-03-01 22:49:55 +0800

    1. add deploy sh 2. add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+### 个人Blog
+- 用[hugo](https://gohugo.io/)静态生成器生成
+- 主题采用[hugo-pacman-theme](https://themes.gohugo.io/hugo-pacman-theme/)
+- 地址：[pengganyu Blog](https://kedadiannao220.github.io/)
+
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+echo "---------------- start --------------------"
+
+echo "---------------- remove public-------------"
+rm -rf public/*
+
+echo "---------------- generate public-----------"
+hugo --theme=hugo-pacman-theme --baseUrl="https://kedadiannao220.github.io/"
+
+echo "---------------- cd   public---------------"
+cd public
+
+echo "---------------- commit public-------------"
+git commit -am  "commit blog"
+
+echo "---------------- push public---------------"
+git push
+
+echo "---------------- end  --------------------"
+
+
+
+
