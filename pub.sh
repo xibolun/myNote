afd031b9e6c4d074d7fd4ec23e4473432a8ed3af
彭甘雨 <pengganyu@idcos.com>
2019-10-22 19:38:57 +0800

    commit message: 删除deploy.sh

--- pub.sh
+++ pub.sh
@@ -1,3 +1,8 @@
+##### pub脚本说明
+##### master为文章的主分支
+##### public目录下面是另一个repository，gitpage必须以Master分支来作为发布分支，所以必须另起一个repository
+##### 将public目录删除，然后重新生成，提交至public的master分支，即可发布
+##### 发布会有延时
 echo "---------------- start --------------------"
 git add .
 
